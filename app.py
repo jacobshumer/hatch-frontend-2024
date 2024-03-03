639bb4599213279519d628bd89055dbbb5c57fed
Uriah Chumpitaz <117397789+L3V1ATHAN97@users.noreply.github.com>
2024-03-03 12:46:40 -0600

    Changes by all

--- app.py
+++ app.py
@@ -1,34 +1,85 @@
-from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+from functools import wraps
+from io import BytesIO
+
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
 import requests
 
+file = open('config.json')
+config = json.load(file)
+backend_url = f'{config['backend']['protocol']}://{config['backend']['host']}:{config['backend']['port']}'
+print(backend_url)
+
 app = Flask(__name__)
+app.secret_key = config['secret_key']
+
+# Dataset of allowed users
+# allowed_users = {
+#    'user1': {'password': 'password1', 'email': 'user1@example.com', 'tfa': 'Phone Number', 'tfaAnswer': '1231231233'},
+#    'user2': {'password': 'password2', 'email': 'user2@example.com', 'tfa': 'Phone Number', 'tfaAnswer': '1231231233'}
+# }
 
 
-#Dataset of allowed users
-allowed_users = {
-    'user1': {'password': 'password1', 'email': 'user1@example.com', 'tfa': 'Phone Number', 'tfaAnswer': '1231231233'},
-    'user2': {'password': 'password2', 'email': 'user2@example.com', 'tfa': 'Phone Number', 'tfaAnswer': '1231231233'}
-}
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        print(session)
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            print('not logged in accessing dashboard page, redirecting to dashboard')
+            return redirect(url_for('login'))
+    return wrap
+
+
+def is_not_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        print(session)
+        if 'logged_in' in session:
+            print('logged in accessing login page, redirecting to dashboard')
+            return redirect(url_for('dashboard'))
+        else:
+            return f(*args, **kwargs)
+    return wrap
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return redirect(url_for('login'))
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
+@is_not_logged_in
 def login():
-    username = request.form['username']
-    password = request.form['password']
-    response = requests.get('http://localhost:5000', data={"username": username, "password": password})
-    if response.status_code == 200:
-        return redirect(urlfor)
-    if username in allowed_users and allowed_users[username]['password'] == password:
-        # Authentication successful, redirect to the next page
-        return redirect(url_for('dashboard', username=username))
-    else:
-        # Authentication failed, redirect back to the sign-in page
-        return redirect(url_for('home'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Allows the session variables to be set without the backend server running
+        print(config)
+        if config['debug']:
+            session['logged_in'] = True
+            session['username'] = username
+            session['token'] = 'token'
+            print(session)
+
+            return redirect(url_for('dashboard'))
+
+        # Gets the data from the server and uses it to set the session variables (or not)
+        res = requests.get(
+            f'{backend_url}/authentication',
+            data={"username": username, "password": password})
+        if res.status_code == 200:
+            session['is_logged_in'] = True
+            session['username'] = username
+            session['token'] = res.json()['token']
+            return redirect(url_for('dashboard', username=username))
+        else:
+            return render_template('error.html', code=res.status_code)
+
+    return render_template('index.html')
 
 
 @app.route('/dashboard', methods=['GET'])
@@ -66,4 +117,6 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.secret_key = config.secret_key
+    print('Starting...')
+    app.run(debug=True, port=config['flask_port'])
